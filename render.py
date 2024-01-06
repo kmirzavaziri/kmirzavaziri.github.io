a95f06ad88cbab8ac44ac18edad372b1586d4b94
Kamyar Mirzavaziri <kamyar.mirzavaziri@divar.ir>
2024-01-06 08:54:52 +0330

    add machine readable resume

--- render.py
+++ render.py
@@ -2,11 +2,19 @@ import yaml
 
 import jinja2
 
-with open("info.yaml", "r") as f:
-    info = yaml.safe_load(f)
 
-templateEnv = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath="templates"))
-template = templateEnv.get_template("index.html.j2")
+def main():
+    with open("info.yaml", "r") as f:
+        info = yaml.safe_load(f)
 
-with open("public/index.html", "w") as f:
-    f.write(template.render(info=info))
+    templateEnv = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath="templates"))
+
+    with open("public/index.html", "w") as f:
+        f.write(templateEnv.get_template("index.html.j2").render(info=info))
+
+    with open("public/assets/KamyarMirzavaziriResumeMR.html", "w") as f:
+        f.write(templateEnv.get_template("resume.html.j2").render(info=info))
+
+
+if __name__ == "__main__":
+    main()
